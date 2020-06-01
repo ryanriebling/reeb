4bb716b013b19fc01e3b4999ccaeaa1bad29b4de
ryanriebling <ryanriebling@gmail.com>
2020-05-31 20:29:52 -0700

    Deno React SSR application

---
+++ dep.ts
@@ -0,0 +1,10 @@
+export { default as React } from 'https://dev.jspm.io/react@16.13.1';
+export {
+  default as ReactDOMServer,
+} from 'https://dev.jspm.io/react-dom@16.13.1/server';
+export { opine } from 'https://deno.land/x/opine@0.4.0/mod.ts';
+export {
+  Request,
+  Response,
+  NextFunction,
+} from 'https://deno.land/x/opine@0.4.0/src/types.ts';
